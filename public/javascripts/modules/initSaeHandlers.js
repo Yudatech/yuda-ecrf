22a3e5b453e39bc77877c3c3191a78d66be0bf27
Wenyuan Sun <wenyuan.sun1982@gmail.com>
2017-09-28 10:12:35 +0200

    #99 fix

--- public/javascripts/modules/initSaeHandlers.js
+++ public/javascripts/modules/initSaeHandlers.js
@@ -1,5 +1,6 @@
 import setFieldVisibility from './helpers/setFieldVisibility';
 import requireValidator from './validators/requireValidator';
+import setErrorStyle from './helpers/setErrorStyle';
 
 function initSaeHandlers() {
   const saecaus_9El = $('#saecaus_9');
@@ -14,6 +15,15 @@ function initSaeHandlers() {
 
   $('#saeres_1').change(function(){
     setSaeres2Visibility();
+  });
+  
+  $('#saestdtc_date').datepicker().on('changeDate', function(e){
+    $('#saecaus_2').datepicker('setStartDate', e.date);
+    setSaecause2ErrorStyle();
+  });
+
+  $('#saecaus_2').datepicker().on('changeDate', function(e){
+    setSaecause2ErrorStyle();
   });
 
   $('#sae-form').validator({
@@ -34,6 +44,19 @@ function setSaecaus10Visibility() {
 function setSaecaus2Visibility() {
   const checked = $('#saecaus_1').is(':checked');
   setFieldVisibility('saecaus_2', checked);
+}
+
+function setSaecause2ErrorStyle() {
+  const startDate = $('#saecaus_2').datepicker('getStartDate');
+  const date = $('#saecaus_2Real').val();
+  let dateValue = null;
+  if (date !== '') {
+    const [month, day, year] = date.split('/');
+    const monthReal = parseInt(month, 10) - 1;
+    dateValue = new Date(year, monthReal, day, 0, 0, 0).valueOf();
+  }
+  const showErrorStyle = startDate !== null && dateValue !== null && (startDate.valueOf() > dateValue);
+  setErrorStyle('saecaus_2', showErrorStyle);
 }
 
 function setSaeres2Visibility() {
