e57907fa79dfeb217875476311e05344da2d3272
Wenyuan Sun <wenyuan.sun1982@gmail.com>
2019-11-14 19:18:29 +0100

    change back to en

--- config/getLoginConfig.js
+++ config/getLoginConfig.js
@@ -3,31 +3,31 @@ const getOptionsLang = require('./configHelpers').getOptionsLang;
 const config = {
   title: {
     zh: '欢迎登录CREX eCRF系统',
-    en: '欢迎登陆 郁达科技 产品展示网站'
+    en: 'Welcome to CREX eCRF system'
   },
   labels: [{
     name: 'userabbr',
     text: {
       zh: '用户名',
-      en: '用户名'
+      en: 'Username'
     }
   }, {
     name: 'password',
     text: {
       zh: '密码',
-      en: '密码'
+      en: 'Password'
     }
   }],
   buttons: [{
     name: 'login',
     text: {
       zh: '登录',
-      en: '登录'
+      en: 'Login'
     }
   }]
 };
 
-module.exports = function(lang) {
+module.exports = function (lang) {
   if (lang === undefined) {
     lang = 'zh';
   }
--- helpers.js
+++ helpers.js
@@ -18,12 +18,12 @@ exports.staticMap = ([lng, lat]) => `https://maps.googleapis.com/maps/api/static
 exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
 // Some details about the site
-exports.siteName = `郁达科技术`;
+exports.siteName = `eCRF`;
 
 exports.menu = [
-  {slug: '/stores', title: 'Stores', icon: 'store'},
-  {slug: '/tags', title: 'Tags', icon: 'tag'},
-  {slug: '/top', title: 'Top', icon: 'top'},
-  {slug: '/add', title: 'Add', icon: 'add'},
-  {slug: '/map', title: 'Map', icon: 'map'}
+  { slug: '/stores', title: 'Stores', icon: 'store' },
+  { slug: '/tags', title: 'Tags', icon: 'tag' },
+  { slug: '/top', title: 'Top', icon: 'top' },
+  { slug: '/add', title: 'Add', icon: 'add' },
+  { slug: '/map', title: 'Map', icon: 'map' }
 ];
