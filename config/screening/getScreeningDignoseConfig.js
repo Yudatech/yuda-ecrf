308df70eab7a83ba5b87760477233e25baaa934b
Wenyuan Sun <wenyuan.sun1982@gmail.com>
2020-08-08 22:13:17 +0200

    #160

--- config/screening/getScreeningDignoseConfig.js
+++ config/screening/getScreeningDignoseConfig.js
@@ -8,6 +8,10 @@ const config = {
   formConfigs: [{
     name: 'dignose_1',
     type: 'textarea',
+    required: true,
+    commit: [{
+      rule: 'required'
+    }],
     text: {
       zh: '手术诊断/指征',
       en: 'Diagnosis/Indication for surgery'
