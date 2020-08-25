095d58918bbf6a739ee7223f0e884b4689880706
suzhang2020 <69921295+suzhang2020@users.noreply.github.com>
2020-08-25 09:11:00 +0200

    Update sv

--- config/getSearchConfig.js
+++ config/getSearchConfig.js
@@ -5,13 +5,14 @@ const config = {
     title: {
       zh: '病例',
       en: 'Cases',
-      sv: 'svenska',
+      sv: 'Patientfall',
     },
     headers: [{
       name: '_id',
       text: {
         zh: '受试者识别编号',
-        en: 'Subject ID'
+        en: 'Subject ID',
+        sv: 'Identifiering av patient'
       }
     }, /*{
       name: 'subjabbr',
@@ -23,68 +24,79 @@ const config = {
       name: 'createDate',
       text: {
         zh: '建档日期',
-        en: 'Create Date'
+        en: 'Create Date',
+        sv: 'Skapad datum'
       }
     }, {
       name: 'username',
       text: {
         zh: '建档人',
-        en: 'User name'
+        en: 'User name',
+        sv: 'Skapade av'
       }
     }, {
       name: 'status',
       text: {
         zh: '状态',
-        en: 'Status'
+        en: 'Status',
+        sv: 'Status'
       }
     }, {
       name: 'operations',
       text: {
         zh: '操作',
-        en: 'Operations'
+        en: 'Operations',
+        sv: 'Operationer'
       }
     }]
   },
   questions: {
     title: {
       zh: '质疑',
-      en: 'Queries'
+      en: 'Queries',
+      sv: 'Förfrågor'
     },
     headers: [{
       name: '_id',
       text: {
         zh: '受试者识别编号',
-        en: 'Subject ID'
+        en: 'Subject ID',
+        sv: 'Identifiering av patient'
       }
     }, {
       name: 'numOfDays',
       text: {
         zh: '天数',
-        en: 'Number of days'
+        en: 'Number of days',
+        sv: 'Antal dagar'
       }
     }, {
       name: 'orig',
       text: {
         zh: '发起人',
-        en: 'Initiator'
+        en: 'Initiator',
+        sv: 'Initiativtagare'
       }
     }, {
       name: 'exec',
       text: {
         zh: '执行人',
-        en: 'Assignee'
+        en: 'Assignee',
+        sv: 'Tilldelad'
       }
     }, {
       name: 'status',
       text: {
         zh: '状态',
-        en: 'Status'
+        en: 'Status',
+        sv: 'Status'
       }
     }, {
       name: 'operations',
       text: {
         zh: '操作',
-        en: 'Operations'
+        en: 'Operations',
+        sv: 'Operationer'
       }
     }]
   },
@@ -92,51 +104,59 @@ const config = {
     name: 'message',
     text: {
       zh: '确认删除',
-      en: 'Confirm to remove'
+      en: 'Confirm to remove',
+      sv: 'Bekräfta vid bortaggning'
     }
   }],
   formConfigs: [{
     name: 'searchinput',
     text: {
       zh: '输入病例号进行搜索',
-      en: 'Input Patient Number'
+      en: 'Input Patient Number',
+      sv: 'Patients identificeringsnummer'
     }
   }, {
     name: 'cra',
     text: {
       zh: '建档人',
-      en: 'Created By'
+      en: 'Created By',
+      sv: 'Skapade av'
     }
   }, {
     name: 'site',
     text: {
       zh: '中心',
-      en: 'Site'
+      en: 'Site',
+      sv: 'Institutionen'
     }
   }, {
     name: 'status',
     text: {
       zh: '病例状态',
-      en: 'Case Status'
+      en: 'Case Status',
+      sv: 'Ärendestatus'
     }
   }],
   searchResult: [{
     name: 'foundcase_0',
     text: {
       zh: '搜索到 ',
-      en: 'Found'
+      en: 'Found',
+      sv: 'Hittat'
     }
   }, {
     name: 'foundcase_1',
     text: {
       zh: ' 条病例，',
-      en: 'cases, '
+      en: 'cases, ',
+      sv: 'patientfall'
     }
   }, {
     name: 'foundquestion',
     text: {
       zh: ' 条对应的质疑',
-      en: 'queries.'
+      en: 'queries.',
+      sv: 'förfrågor'
     }
   }]
 };
