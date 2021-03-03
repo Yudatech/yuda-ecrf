1a6ba92349501517811c178cc47cb4c5e56f8ffd
Wenyuan Sun <wenyuan.sun1982@gmail.com>
2021-03-03 20:23:18 +0100

    Add model

--- start.js
+++ start.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
-const http = require("http");
-const https = require("https");
-const fs = require("fs");
+const mongoose = require('mongoose');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
 // Make sure we are running node 7.6+
-const [major, minor] = process.versions.node.split(".").map(parseFloat);
+const [major, minor] = process.versions.node.split('.').map(parseFloat);
 if (major < 7 || (major === 7 && minor <= 5)) {
   console.log(
     "🛑 🌮 🐶 💪 💩\nHey You! \n\t ya you! \n\t\tBuster! \n\tYou're on an older version of node that doesn't support the latest and greatest things we are learning (Async + Await)! Please go to nodejs.org and download version 7.6 or greater. 👌\n "
@@ -13,37 +13,38 @@ if (major < 7 || (major === 7 && minor <= 5)) {
 }
 
 // import environmental variables from our variables.env file
-require("dotenv").config({ path: "variables.env" });
+require('dotenv').config({ path: 'variables.env' });
 
 // Connect to our Database and handle an bad connections
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
-mongoose.connection.on("error", (err) => {
+mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
 // READY?! Let's go!
 
 // import all models
-require("./models/User");
-require("./models/Site");
-require("./models/Case");
-require("./models/Screening");
-require("./models/ReviewChecklist");
-require("./models/Discontinuation");
-require("./models/Surgery");
-require("./models/Life");
-require("./models/Ae");
-require("./models/Sae");
-require("./models/Visit");
-require("./models/Question");
-require("./models/History");
-require("./models/Evacuation");
-require("./models/Pathological");
-require("./models/Followup");
+require('./models/User');
+require('./models/Site');
+require('./models/Case');
+require('./models/Screening');
+require('./models/ReviewChecklist');
+require('./models/Discontinuation');
+require('./models/Surgery');
+require('./models/Life');
+require('./models/Ae');
+require('./models/Sae');
+require('./models/Visit');
+require('./models/Question');
+require('./models/History');
+require('./models/Evacuation');
+require('./models/Pathological');
+require('./models/Followup');
+require('./models/EvacuationFollowup');
 
 // Start our app!
-const app = require("./app");
+const app = require('./app');
 // app.set('port', process.env.PORT || 7777);
 // const server = app.listen(app.get('port'), () => {
 //   console.log(`Express running → PORT ${server.address().port}`);
@@ -57,13 +58,11 @@ const app = require("./app");
 
 // start https server
 let sslOptions = {
-  key: fs.readFileSync("cert/yuda-ecrf.key"),
-  cert: fs.readFileSync("cert/yuda-ecrf.cert"),
-  ca: fs.readFileSync("cert/yuda-ecrf.ca-bundle"),
+  key: fs.readFileSync('cert/yuda-ecrf.key'),
+  cert: fs.readFileSync('cert/yuda-ecrf.cert'),
+  ca: fs.readFileSync('cert/yuda-ecrf.ca-bundle'),
 };
 
-let serverHttps = https
-  .createServer(sslOptions, app)
-  .listen(process.env.HTTPS_PORT || 443, () => {
-    console.log(`Express running → PORT ${serverHttps.address().port}`);
-  });
+let serverHttps = https.createServer(sslOptions, app).listen(process.env.HTTPS_PORT || 443, () => {
+  console.log(`Express running → PORT ${serverHttps.address().port}`);
+});
