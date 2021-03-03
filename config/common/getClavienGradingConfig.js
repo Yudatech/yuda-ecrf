5e15d66349c3c6496cec96ed264e7582e750621c
Wenyuan Sun <wenyuan.sun1982@gmail.com>
2021-03-03 19:01:47 +0100

    text fix

--- config/common/getClavienGradingConfig.js
+++ config/common/getClavienGradingConfig.js
@@ -1,56 +1,64 @@
-const clavienGradingConfig = [{
-  value: 0,
-  text: {
-    zh: 'No',
-    en: 'No'
-  }
-}, {
-  value: 1,
-  text: {
-    zh: '2',
-    en: '2'
-  }
-}, {
-  value: 2,
-  text: {
-    zh: '3a',
-    en: '3a'
-  }
-}, {
-  value: 3,
-  text: {
-    zh: '3b',
-    en: '3b'
-  }
-}, {
-  value: 4,
-  text: {
-    zh: '4a',
-    en: '4a'
-  }
-}, {
-  value: 5,
-  text: {
-    zh: '4b',
-    en: '4b'
-  }
-}, {
-  value: 6,
-  text: {
-    zh: '5(dead)',
-    en: '5(dead)'
-  }
-}];
+const clavienGradingConfig = [
+  {
+    value: 0,
+    text: {
+      zh: "No",
+      en: "No",
+    },
+  },
+  {
+    value: 1,
+    text: {
+      zh: "2",
+      en: "2",
+    },
+  },
+  {
+    value: 2,
+    text: {
+      zh: "3a",
+      en: "3a",
+    },
+  },
+  {
+    value: 3,
+    text: {
+      zh: "3b",
+      en: "3b",
+    },
+  },
+  {
+    value: 4,
+    text: {
+      zh: "4a",
+      en: "4a",
+    },
+  },
+  {
+    value: 5,
+    text: {
+      zh: "4b",
+      en: "4b",
+    },
+  },
+  {
+    value: 6,
+    text: {
+      zh: "(5)",
+      en: "(5)",
+    },
+  },
+];
 
 module.exports = function (lang) {
   if (lang === undefined) {
-    lang = 'en';
+    lang = "en";
   }
 
   return clavienGradingConfig.map((item) => {
     return {
       value: item.value,
-      text: item.text[lang]
+      text: item.text[lang],
     };
   });
 };
--- config/getReviewChecklistConfig.js
+++ config/getReviewChecklistConfig.js
@@ -1,78 +1,97 @@
-const getOptionsLang = require('./configHelpers').getOptionsLang;
+const getOptionsLang = require("./configHelpers").getOptionsLang;
 
 const config = {
   title: {
-    zh: '复诊（入组）',
-    en: 'Prior to Surgery',
-    sv: 'Dagen före operation'
+    zh: "复诊（入组）",
+    en: "Prior to Surgery",
+    sv: "Dagen före operation",
   },
   subtitles: [
     {
-      name: 'subtitle_1',
+      name: "subtitle_1",
       text: {
-        zh: '复诊（入组）',
-        en: 'Prior to surgery'
-      }
-    }
+        zh: "复诊（入组）",
+        en: "Prior to surgery",
+      },
+    },
   ],
-  formConfigs: [{
-    name: 'reviewcheckdate',
-    type: 'date',
-    required: true,
-    commit: [{
-      rule: 'date',
-      start: 'subjAcceptDate',
-      end: 'now'
-    }],
-    text: {
-      zh: '复诊日期',
-      en: 'Date',
-      sv: 'Datum för dagen före operation'
-    }
-  }, {
-    name: 'reviewcheck_2',
-    type: 'checkbox',
-    commit: [{
-      rule: 'must_true'
-    }],
-    text: {
-      zh: '肠道清洁',
-      en: 'Full bowel cleansing (3-4 liter Movprep the evening before surgery and ad 1 liter in the morning as an option)',
-      sv: 'Fullständig tarmregöring'
-    }
-  }, {
-    name: 'reviewcheck_3',
-    type: 'checkbox',
-    commit: [{
-      rule: 'must_true'
-    }],
-    text: {
-      zh: '预防性抗生素 (若使用抗生素，请完整填写《合并用药情况表》)',
-      en: 'Prophylactic Antibiotics (T. Bactrim forte/Eusaprim forte 1 x 1 and T. Metronidazole 500 mg 3 x 1, 2–4 h before surgery (if sulfa allergy give Zinacef 1.5 gx 1 IV and Metronidazole 1 gx 1 IV 30–60 min before surgery) If surgery is delayed more than 8 h after given T. Bactrim forte give a new dose prior surgery)'
-    }
-  }, {
-    name: 'reviewcheck_4',
-    type: 'checkbox',
-    commit: [{
-      rule: 'must_true'
-    }],
-    text: {
-      zh: '预防性抗凝血药 (若使用抗凝血药，请完整填写《合并用药情况表》)',
-      en: 'Trombosprofylax (Prophylactic Anti-Thrombotic therapy = Inj Innohep 4500 E /Fragmin 5000 E evening before surgery) '
-    }
-  }],
-  errors: [{
-    name: 'error_1',
-    text: {
-      zh: '患者应完成上述所有术前准备和药物治疗',
-      en: 'The patient should fullfill ALL of the preoperative preparation and medications above.'
-    }
-  }]
+  formConfigs: [
+    {
+      name: "reviewcheckdate",
+      type: "date",
+      required: true,
+      commit: [
+        {
+          rule: "date",
+          start: "subjAcceptDate",
+          end: "now",
+        },
+      ],
+      text: {
+        zh: "复诊日期",
+        en: "Date",
+        sv: "Datum för dagen före operation",
+      },
+    },
+    {
+      name: "reviewcheck_2",
+      type: "checkbox",
+      commit: [
+        {
+          rule: "must_true",
+        },
+      ],
+      text: {
+        zh: "肠道清洁",
+        en:
+          "Complete bowel cleansing ((4000 ml Laxabon or 2000 ml Movprep) the evening before surgery + possibly another liter in the morning.)",
+        sv: "Fullständig tarmregöring",
+      },
+    },
+    {
+      name: "reviewcheck_3",
+      type: "checkbox",
+      commit: [
+        {
+          rule: "must_true",
+        },
+      ],
+      text: {
+        zh: "预防性抗生素 (若使用抗生素，请完整填写《合并用药情况表》)",
+        en:
+          "Antibiotic prophylaxis (T. Bactrim forte / Eusaprim forte 1 x 1 and T. Metronidazole 500 mg 3 x 1, 2–4 h before surgery (if allergy to sulfa give Zinacef 1.5 g x 1 iv and Metronidazole 1 g x 1 iv 30–60 min before surgery). (If surgery is delayed more than 8 hours, supplement with additional Bactrim or Zinacef, according to guidelines in FASS))",
+      },
+    },
+    {
+      name: "reviewcheck_4",
+      type: "checkbox",
+      commit: [
+        {
+          rule: "must_true",
+        },
+      ],
+      text: {
+        zh: "预防性抗凝血药 (若使用抗凝血药，请完整填写《合并用药情况表》)",
+        en:
+          "Thrombosis prevention (Prophylactic Antithrombotic therapy = In Innohep 4500 E / Fragmin 5000 E evening before surgery) ",
+      },
+    },
+  ],
+  errors: [
+    {
+      name: "error_1",
+      text: {
+        zh: "患者应完成上述所有术前准备和药物治疗",
+        en:
+          "The patient should fullfill ALL of the preoperative preparation and medications above.",
+      },
+    },
+  ],
 };
 
 module.exports = function (lang) {
   if (lang === undefined) {
-    lang = 'en';
+    lang = "en";
   }
 
   const result = {};
